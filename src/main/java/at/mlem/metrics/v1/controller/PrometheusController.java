e44a71f92d054a1c0865a2d6e504aa7cf847e23e
Martin Lemanski <martin.lemanski@willhaben.at>
2019-11-08 10:50:06 +0100

    first solution for https://github.com/prometheus/prometheus/issues/6284
    But not working yet :(

--- src/main/java/at/mlem/metrics/v1/controller/PrometheusController.java
+++ src/main/java/at/mlem/metrics/v1/controller/PrometheusController.java
@@ -2,11 +2,16 @@ package at.mlem.metrics.v1.controller;
 
 import io.micrometer.prometheus.PrometheusMeterRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.nio.charset.StandardCharsets;
 
 @Controller
@@ -23,8 +28,18 @@ public class PrometheusController {
     @GetMapping(value = "/prometheus", produces = {"application/openmetrics-text;charset=UTF-8"})
     @ResponseBody
     @SuppressWarnings("unused")
-    public String getMetrics(){
-        return new String(registry.scrape().getBytes(StandardCharsets.UTF_8), StandardCharsets.UTF_8);
+    public ResponseEntity<String> getMetrics(HttpServletRequest request, HttpServletResponse response) {
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.add("Content-Type", "application/openmetrics-text;charset=utf-8");
+
+        return new ResponseEntity<>(
+                new String(
+                        registry.scrape().getBytes(StandardCharsets.ISO_8859_1),
+                        StandardCharsets.UTF_8
+                ),
+                responseHeaders,
+                HttpStatus.OK);
     }
+
 
 }
