4dab9d02ecb29d9373251e9a4a65c1459028a55f
Martin Lemanski <martin.lemanski@willhaben.at>
2019-11-08 11:15:00 +0100

    fixed by providing the correct headers

--- src/main/java/at/mlem/metrics/v1/controller/PrometheusController.java
+++ src/main/java/at/mlem/metrics/v1/controller/PrometheusController.java
@@ -2,17 +2,10 @@ package at.mlem.metrics.v1.controller;
 
 import io.micrometer.prometheus.PrometheusMeterRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.nio.charset.StandardCharsets;
 
 @Controller
 @RequestMapping
@@ -25,20 +18,11 @@ public class PrometheusController {
 
     private final PrometheusMeterRegistry registry;
 
-    @GetMapping(value = "/prometheus", produces = {"application/openmetrics-text;charset=UTF-8"})
+    @GetMapping(value = "/prometheus", produces = {"text/plain;version=0.0.4;charset=utf-8"})
     @ResponseBody
     @SuppressWarnings("unused")
-    public ResponseEntity<String> getMetrics(HttpServletRequest request, HttpServletResponse response) {
-        HttpHeaders responseHeaders = new HttpHeaders();
-        responseHeaders.add("Content-Type", "application/openmetrics-text;charset=utf-8");
-
-        return new ResponseEntity<>(
-                new String(
-                        registry.scrape().getBytes(StandardCharsets.ISO_8859_1),
-                        StandardCharsets.UTF_8
-                ),
-                responseHeaders,
-                HttpStatus.OK);
+    public String getMetrics() {
+        return registry.scrape();
     }
 
 
